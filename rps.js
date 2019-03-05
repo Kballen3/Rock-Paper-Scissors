de01758e956028ca40a92bb05f96bb20453c08ab
Kaleb Allen <7kaleb29allen@gmail.com>
2019-03-05 07:55:23 -0700

    commit

---
+++ rps.js
@@ -0,0 +1,35 @@
+document.body.style.background = 'red';
+// set choice to variables
+// user option
+// computer option
+// compare results with case argument also use switch argument
+
+var options = ["Rock", "Paper", "Scissors"]
+    
+var userChoice = 
+
+var userChoice = document.getElementById('rock')
+ = options[0]
+rock.addEventListener("click", function userChoice() {
+})
+var paper = document.getElementById('paper')
+paper.addEventListener("click", function userChoice() {
+    userChoice = options[1]
+})
+var scissors = document.getElementById('scissors')
+scissors.addEventListener("click", function userChoice() {
+    userChoice = options[2]
+})
+
+
+
+
+
+
+
+
+
+// var rock = document.getElementsByClassName("choice-rock");
+// rock.addEventListener("click", )
+// var Paper = document.getElementsByClassName("choice-paper");
+// var Scissors = document.getElementsByClassName("choice-scissors");
